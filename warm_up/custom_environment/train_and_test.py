1d01f5c7f54857b17e4ea89558d458d2a258a477
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-09-10 15:56:53 +0200

    Update train and test file

--- warm_up/custom_environment/train_and_test.py
+++ warm_up/custom_environment/train_and_test.py
@@ -1,16 +1,47 @@
 from AvocadoRunEnv import AvocadoRunEnv
+from AvocadoRunGymEnv import AvocadoRunGymEnv
 from QLAgent import QLAgent
 from DQLAgent import DQLAgent
+from HumanPlayer import HumanPlayer
 
-ql_env = AvocadoRunEnv(moving_enemy=True)
-dql_env = AvocadoRunEnv(observations_as_images=True,
-                        moving_enemy=True, num_enemies=2)
 
-ql_agent = QLAgent(ql_env)
-dql_agent = DQLAgent(dql_env)
+env = AvocadoRunGymEnv(
+    # Whether the enemies should move (partly towards the agent)
+    moving_enemy=True,
 
-# ql_agent.train(episodes=1_000_000)
-# ql_agent.test("qtables/qtable-1724926991.pickle")
-# dql_agent.train(500)
-dql_agent.test(
-    "models/256x2__500.00max__494.10avg__470.00min__1724893683.keras")
+    # The number of enemies
+    num_enemies=2,
+
+    # The render mode. Can be None, "human" or "q_values"
+    render_mode="q_values",
+
+    # If render mode is either "human" or "q_values", denotes how many episodes should pass before showing a game
+    show_every=1,
+
+    # Frames per second of the rendering of the game, If render mode is either "human" or "q_values"
+    render_fps=4
+)
+
+dql_agent = DQLAgent(
+    env=env,
+    replay_buffer_size=50_000,
+    min_replay_buffer_size=1_000,
+    minibatch_size=64,
+    discount=0.99,
+    update_target_every=5,
+    epsilon=1,
+    epsilon_decay=0.99975,
+    min_epsilon=0.001,
+    # "models/256x2___30.00max___-9.18avg_-314.00min__1725863125.keras"
+    model_path="models/256x2___30.00max___-9.18avg_-314.00min__1725863125.keras"
+)
+dql_agent.train(
+    episodes=20_000,
+    rolling_average_window=50,
+    min_save_model_episode=5_000,
+    save_model_every=100
+)
+dql_agent.test()
+
+# human = HumanPlayer(env)
+# human.play()
