333db97a16f263faac4212e9ee03472dd795fccb
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-09-21 19:10:58 +0200

    Update test and train scripts

--- test.py
+++ test.py
@@ -3,14 +3,15 @@ import avocado_run
 from avocado_run.wrappers import QValueRenderWrapper
 from DoubleDQNAgent import DoubleDQNAgent
 
-env = gym.make(id="AvocadoRun-v0", render_mode="human")
+env = gym.make(id="AvocadoRun-v0", render_mode="human", num_avocados=1)
 
 agent = DoubleDQNAgent(
     env=env,
-    model_path="models/64x2___99.00max__-91.65avg_-160.00min__1726327515.keras"
+    model_path="models/wild-glitter-14/model___99.23avg___99.90max___98.10min__1726876253.keras"
 )
 
-wrapped_env = QValueRenderWrapper(env=env, model=agent.online_model)
+wrapped_env = QValueRenderWrapper(
+    env=env, model=agent.online_model, render_fps=1)
 
-# agent.test(episodes=5, env=env)
-agent.test(episodes=5, env=wrapped_env)
+agent.test(episodes=10, env=env)
+# agent.test(episodes=10, env=wrapped_env)
--- train.py
+++ train.py
@@ -3,19 +3,23 @@ import avocado_run
 from DoubleDQNAgent import DoubleDQNAgent
 
 config = {
-    # Not relevant for testing
+    "project_name": "AvocadoRun",
+    "step_penalty": -0.1,
+    "enemy_hit_penalty": -20,
+    "avocado_reward": 100,
+
     "replay_buffer_size": 100_000,
     "min_replay_buffer_size": 10_000,
     "minibatch_size": 64,
     "discount": 0.95,
     "training_frequency": 16,
-    "update_target_every": 2_000,
+    "update_target_every": 2000,
     "learning_rate": 0.001,
+    "prop_steps_epsilon_decay": 0.9,  # The proportion of steps epsilon should decay
     "starting_epsilon": 1,
-    "epsilon_decay": 0.99985,
     "min_epsilon": 0.01,
-    "episodes_to_train": 3_000,
-    "average_window": 100,
+    "steps_to_train": 2_000_000,
+    "episode_metrics_window": 100,  # Number of episodes to take into account for metrics
 }
 
 env = gym.make(id="AvocadoRun-v0", render_mode="human")
@@ -26,4 +30,4 @@ agent = DoubleDQNAgent(
     learning_rate=config["learning_rate"]
 )
 
-agent.train(config=config, track_metrics=False)
+agent.train(config=config, track_metrics=True)
