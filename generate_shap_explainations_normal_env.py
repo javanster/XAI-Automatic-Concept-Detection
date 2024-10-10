dcba445894d52163c27a86a2233d8ceab6b5dd81
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-10-10 16:40:10 +0200

    SHAP normal env update

--- generate_shap_explainations_normal_env.py
+++ generate_shap_explainations_normal_env.py
@@ -7,8 +7,8 @@ from ObservationHandler import ObservationHandler
 import matplotlib.pyplot as plt
 import os
 
-train_run_name = "wild-glitter-14"
-model_name = "model___99.23avg___99.90max___98.10min__1726876253"
+train_run_names = ["wild_glitter_14", "eager_disco_16"]
+model_names = ["best_model", "best_model"]
 
 action_dict = {
     0: "up",
@@ -20,55 +20,59 @@ action_dict = {
 
 env = gym.make(id="AvocadoRun-v0")
 
-agent = DoubleDQNAgent(
-    env=env,
-    model_path=f"models/{train_run_name}/{model_name}.keras"
-)
+for i in range(len(train_run_names)):
+    train_run_name = train_run_names[i]
+    model_name = model_names[i]
 
-background_observations = ObservationHandler.load_observations(
-    file_path="data/observations/normal_environment/random_observations_normal_env_1728221019.npy", normalize=True)
+    agent = DoubleDQNAgent(
+        env=env,
+        model_path=f"models/{train_run_name}/{model_name}.keras"
+    )
 
-actions = ["up", "right", "down", "left", "do_nothing"]
-observation_focuses = ["avocado", "enemy"]
+    background_observations = ObservationHandler.load_observations(
+        file_path="shap_data/observations/normal_environment/random_observations.npy", normalize=True)
 
-for good_action_for_obs in actions:
-    for observation_focus in observation_focuses:
+    actions = ["up", "right", "down", "left", "do_nothing"]
+    observation_focuses = ["avocado", "enemy"]
 
-        observations_to_explain = ObservationHandler.load_observations(
-            file_path=f"data/observations/normal_environment/{good_action_for_obs}_good_action_{observation_focus}_focused_observations.npy",  normalize=True)
+    for good_action_for_obs in actions:
+        for observation_focus in observation_focuses:
 
-        if len(observations_to_explain) > 0:
-            explainer = DeepExplainer(
-                data=background_observations,
-                model=agent.online_model
-            )
+            observations_to_explain = ObservationHandler.load_observations(
+                file_path=f"shap_data/observations/normal_environment/{good_action_for_obs}_good_action_{observation_focus}_focused_observations.npy",  normalize=True)
 
-            shap_values = explainer.shap_values(observations_to_explain)
-            q_values = agent.online_model.predict(observations_to_explain)
+            if len(observations_to_explain) > 0:
+                explainer = DeepExplainer(
+                    data=background_observations,
+                    model=agent.online_model
+                )
 
-            num_actions = env.action_space.n
-            shap_values_list = []
+                shap_values = explainer.shap_values(observations_to_explain)
+                q_values = agent.online_model.predict(observations_to_explain)
 
-            for action in range(num_actions):
-                shap_values_action = shap_values[..., action]
-                shap_values_list.append(shap_values_action)
+                num_actions = env.action_space.n
+                shap_values_list = []
 
-            labels = [
-                f"Action \"{action_dict[action]}\"" for action in range(num_actions)]
+                for action in range(num_actions):
+                    shap_values_action = shap_values[..., action]
+                    shap_values_list.append(shap_values_action)
 
-            shap_image_plot(
-                shap_values_list=shap_values_list,
-                pixel_values=observations_to_explain,
-                q_values=q_values,
-                labels=labels,
-                show=False
-            )
+                labels = [
+                    f"Action \"{action_dict[action]}\"" for action in range(num_actions)]
 
-            plt.suptitle(
-                f"\"{good_action_for_obs}\" good action, {observation_focus} focused", fontsize=16)
+                shap_image_plot(
+                    shap_values_list=shap_values_list,
+                    pixel_values=observations_to_explain,
+                    q_values=q_values,
+                    labels=labels,
+                    show=False
+                )
 
-            filename = f"{good_action_for_obs}_good_action_{observation_focus}_focused_obs.png"
-            filepath = os.path.join(
-                f"shap_explanations/{train_run_name}/{model_name}/normal_environment/", filename)
-            plt.savefig(filepath, dpi=300, bbox_inches='tight')
-            plt.close()
+                plt.suptitle(
+                    f"\"{good_action_for_obs}\" good action, {observation_focus} focused", fontsize=16)
+
+                filename = f"{good_action_for_obs}_good_action_{observation_focus}_focused_obs.png"
+                filepath = os.path.join(
+                    f"shap_explanations/normal_environment/{train_run_name}/{model_name}/", filename)
+                plt.savefig(filepath, dpi=300, bbox_inches='tight')
+                plt.close()
