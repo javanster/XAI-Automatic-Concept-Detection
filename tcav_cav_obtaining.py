4d7e5a9f04cb418f29161ec071360ac3f9e781ec
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-10-12 18:05:37 +0200

    Update to cav script and obtain cavs and results

--- tcav_cav_obtaining.py
+++ tcav_cav_obtaining.py
@@ -5,56 +5,58 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-import csv
+from ConceptDetector import ConceptDetector
+import pandas as pd
 
-
-LAYER_INDEXES = [0, 1, 3]
-CONCEPTS = [c for c in range(1, 7)]
 TRAIN_RUN_NAME = "eager_disco_16"
 MODEL_NAME = "best_model"
 
 model = load_model(f"models/{TRAIN_RUN_NAME}/{MODEL_NAME}.keras")
 
+LAYER_INDEXES = [l for l in range(len(model.layers))]
+CONCEPTS = [c for c in ConceptDetector.concept_name_dict.keys()]
 
-def get_activations_of_layer(model, layer_index, observations):
-    activation_model = Model(
-        inputs=model.layers[0].input, outputs=model.layers[layer_index].output)
+
+def get_activations(activation_model, observations):
     activations = activation_model.predict(observations)
 
     # If the layer is a Conv2D layer, flatten the output
-    if len(activations.shape) == 4:  # This indicates a 4D tensor: (batch_size, height, width, channels)
+    if len(activations.shape) == 4:
         batch_size, height, width, channels = activations.shape
-        # Flatten the activations to 2D shape: (batch_size, height * width * channels)
         activations = activations.reshape(
             batch_size, height * width * channels)
 
     return activations
 
 
-accuracy_dict = {}
+accuracy_results = []
 
 for layer_index in LAYER_INDEXES:
+
+    activation_model_for_layer = Model(
+        inputs=model.layers[0].input,
+        outputs=model.layers[layer_index].output
+    )
+
     for concept in CONCEPTS:
 
         observations_with_concept = ObservationHandler.load_observations(
-            file_path=f"tcav_data/observations/observations_containing_concept_{concept}.npy",
+            file_path=f"tcav_data/observations/concept_observations/observations_containing_concept_{concept}.npy",
             normalize=True,
         )
 
         random_observations_without_concept = ObservationHandler.load_observations(
-            file_path=f"tcav_data/observations/observations_not_containing_concept_{concept}.npy",
+            file_path=f"tcav_data/observations/concept_observations/observations_not_containing_concept_{concept}.npy",
             normalize=True,
         )
 
-        concept_activations_for_layer = get_activations_of_layer(
-            model=model,
-            layer_index=layer_index,
+        concept_activations_for_layer = get_activations(
+            activation_model=activation_model_for_layer,
             observations=observations_with_concept
         )
 
-        non_concept_activations_for_layer = get_activations_of_layer(
-            model=model,
-            layer_index=layer_index,
+        non_concept_activations_for_layer = get_activations(
+            activation_model=activation_model_for_layer,
             observations=random_observations_without_concept
         )
 
@@ -75,28 +77,24 @@ for layer_index in LAYER_INDEXES:
         y_pred = clf.predict(X_test)
         accuracy = accuracy_score(y_test, y_pred)
 
-        accuracy_dict[f"Concept {concept} - layer {layer_index}"] = accuracy
+        accuracy_results.append(
+            {
+                "concept_index": concept,
+                "concept_name": ConceptDetector.concept_name_dict[concept],
+                "layer_index": layer_index,
+                "layer_name": model.layers[layer_index].name,
+                "classifier_accuracy": accuracy
+            }
+        )
 
         cav = clf.coef_[0]
 
         np.save(
             f"tcav_data/cavs/{TRAIN_RUN_NAME}/{MODEL_NAME}/concept_{concept}_layer_{layer_index}_cav.npy", cav)
 
-print(f"{'Concept':<20} | {'Classifier Accuracy':>18}")
-print('-' * 42)
-for concept in accuracy_dict.keys():
-    print(f"{concept:<20} | {accuracy_dict[concept]}")
+accuracy_results_df = pd.DataFrame(accuracy_results)
 
-with open(
-    f"tcav_data/cavs/{TRAIN_RUN_NAME}/{MODEL_NAME}/accuracy_table.csv",
-    mode='w',
-    newline=''
-) as csv_file:
-    fieldnames = ['Concept', 'Classifier Accuracy']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    for concept in accuracy_dict:
-        writer.writerow({
-            'Concept': concept,
-            'Classifier Accuracy': accuracy_dict[concept]
-        })
+print(accuracy_results_df)
+
+accuracy_results_df.to_csv(
+    f"tcav_data/cavs/{TRAIN_RUN_NAME}/{MODEL_NAME}/cav_accuracies.csv", index=False)
