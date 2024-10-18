5e77eb8f45fda267cf60871fdea8aa150a61832c
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-10-18 10:34:44 +0200

    Change hyperparams to best ones found in sweep

--- train.py
+++ train.py
@@ -2,23 +2,24 @@ import gymnasium as gym
 import avocado_run
 from DoubleDQNAgent import DoubleDQNAgent
 
+# Based on best hyperparams found using Bayesian Hyperparameter Optimization - See wandb sandy-sweep-16
 config = {
     "project_name": "AvocadoRun",
-    "replay_buffer_size": 100_000,
+    "replay_buffer_size": 50_000,
     "min_replay_buffer_size": 10_000,
     "minibatch_size": 64,
     "discount": 0.95,
     "training_frequency": 16,
-    "update_target_every": 2000,
+    "update_target_every": 1000,
     "learning_rate": 0.001,
     "prop_steps_epsilon_decay": 0.9,  # The proportion of steps epsilon should decay
     "starting_epsilon": 1,
-    "min_epsilon": 0.01,
+    "min_epsilon": 0.05,
     "steps_to_train": 2_000_000,
     "episode_metrics_window": 100,  # Number of episodes to take into account for metrics
 }
 
-env = gym.make(id="AvocadoRun-v0", render_mode="human")
+env = gym.make(id="AvocadoRun-v0")
 
 agent = DoubleDQNAgent(
     env=env,
