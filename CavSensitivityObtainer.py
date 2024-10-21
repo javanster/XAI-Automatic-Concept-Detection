47bd01e8825df1ba01f15de44d6e4e125c393fec
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-10-21 09:48:18 +0200

    CavSensitivityObtainers update

--- CavSensitivityObtainer.py
+++ CavSensitivityObtainer.py
@@ -1,15 +1,12 @@
+from sklearn.linear_model import LogisticRegression
 from ObservationHandler import ObservationHandler
 from keras import Model
 import numpy as np
 from sklearn.model_selection import train_test_split
 from ConceptDetector import ConceptDetector
 import pandas as pd
-from keras.api.models import Sequential
-from keras.api.layers import Input, Dense
-from keras import regularizers
-from keras.api.losses import MeanSquaredError
-from keras.api.optimizers import Adam
 from tqdm import tqdm
+from sklearn.metrics import accuracy_score
 
 
 class CavSensitivityObtainer:
@@ -24,7 +21,6 @@ class CavSensitivityObtainer:
 
     @staticmethod
     def _get_activations(model, observations):
-        print(f"Observations shape: {observations.shape}")
         activations = model.predict(observations)
 
         # If the layer is a Conv2D layer, flatten the output
@@ -33,37 +29,7 @@ class CavSensitivityObtainer:
             activations = activations.reshape(
                 batch_size, height * width * channels)
 
-        print(f"Activations shape: {activations.shape}")
         return activations
-
-    def _create_logistic_regression_model(self, input_dim):
-        model = Sequential()
-        model.add(Input(shape=(input_dim,)))
-        model.add(
-            Dense(
-                units=1,
-                activation='sigmoid',
-                kernel_regularizer=regularizers.L1(0.01),
-                bias_regularizer=regularizers.L1(0.01),
-            )
-        )
-
-        return model
-
-    def _calculate_concept_score(self, predictions, labels):
-        # Convert predictions to binary using the threshold of 0.5
-        binary_predictions = np.round(predictions)
-
-        # Calculate the number of correct predictions
-        correct_predictions = np.sum(binary_predictions == labels)
-
-        # Calculate the proportion of correct predictions
-        concept_score = correct_predictions / len(labels)
-
-        # Subtract 0.5 and apply ReLU (max with 0)
-        concept_score_adjusted = max(concept_score - 0.5, 0)
-
-        return concept_score_adjusted
 
     def calculate_cav_sensitivity(self, model, training_step, file_path):
         layer_indexes = [l for l in range(len(model.layers))]
@@ -80,9 +46,6 @@ class CavSensitivityObtainer:
                 )
 
                 for concept in self.concepts:
-
-                    print(
-                        f"Sensitivity for concept {concept} in layer {layer_index} at step {training_step}")
 
                     observations_with_concept = ObservationHandler.load_observations(
                         file_path=self.concept_observations_dict.get(
@@ -116,41 +79,19 @@ class CavSensitivityObtainer:
                     y = np.concatenate(
                         [labels_with_concept, labels_without_concept])
 
-                    print(f"X shape: {X.shape}, y shape: {y.shape}")
-
                     X_train, X_val, y_train, y_val = train_test_split(
                         X, y, test_size=0.2, random_state=42
                     )
 
-                    print(
-                        f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
-                    print(
-                        f"X_val shape: {X_val.shape}, y_val shape: {y_val.shape}")
+                    clf = LogisticRegression(
+                        penalty='l1', solver='saga', C=0.1, max_iter=1000, random_state=42)
+                    clf.fit(X_train, y_train)
 
-                    logistic_model = self._create_logistic_regression_model(
-                        input_dim=X_train.shape[1])
-                    logistic_model.compile(
-                        optimizer=Adam(),
-                        loss=MeanSquaredError(),
-                        metrics=['accuracy']
-                    )
+                    y_pred = clf.predict(X_val)
+                    original_accuracy = accuracy_score(y_val, y_pred)
 
-                    logistic_model.fit(
-                        X_train, y_train,
-                        epochs=100,
-                        batch_size=32,
-                        validation_data=(X_val, y_val),
-                        verbose=0
-                    )
-
-                    predictions = logistic_model.predict(
-                        X_val).flatten()
-
-                    print(f"Predictions shape: {predictions.shape}")
-                    print(f"y_val shape: {y_val.shape}")
-
-                    cav_sensitivity = self._calculate_concept_score(
-                        predictions, y_val)
+                    # Ensures score is between 0 and 1, where 0 is equal to random guess or worse
+                    cav_sensitivity = max(2 * (original_accuracy - 0.5), 0)
 
                     cav_sensitivities.append(
                         {
