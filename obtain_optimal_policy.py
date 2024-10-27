02d2850a8a77b0c125589e9db1dfbb22db195db7
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-10-27 15:28:57 +0100

    Scripts for obtaining and testing optimal policy

---
+++ obtain_optimal_policy.py
@@ -0,0 +1,16 @@
+from agents import ValueIterationAgent
+import gymnasium as gym
+import avocado_run
+
+
+env = gym.make(id="AvocadoRun-v0", render_mode="human",
+               num_avocados=1, num_enemies=1, aggressive_enemies=False)
+
+agent = ValueIterationAgent(env)
+
+agent.find_optimal_policy(
+    gamma=0.99,
+    theta=1e-6,
+    max_iterations=1000,
+    file_path="optimal_policy/optimal_policy.npy",
+)
---
+++ test_optimal_policy.py
@@ -0,0 +1,16 @@
+from agents import ValueIterationAgent
+import gymnasium as gym
+import avocado_run
+
+
+env = gym.make(id="AvocadoRun-v0", render_mode="human",
+               num_avocados=1, num_enemies=1, aggressive_enemies=False)
+
+agent = ValueIterationAgent(env)
+
+agent.test_policy(
+    policy_file_path="optimal_policy/optimal_policy.npy",
+    episodes=1_000_000,
+    render=False,
+    verify_never_caught=True
+)
