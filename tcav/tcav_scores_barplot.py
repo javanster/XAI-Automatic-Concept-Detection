5ea71a4200c04974900b76d2378eebca5c4ac1d4
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-11-03 12:20:30 +0100

    Move TCAV scores barplot to package, add documentation and add ensurance that filepath exists

--- tcav/tcav_scores_barplot.py
+++ tcav/tcav_scores_barplot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import os
 
 sns.set_theme(style="darkgrid")
 
@@ -9,6 +10,34 @@ def tcav_scores_barplot(df, target_class_name, show=True, file_path_for_saving=N
     """
     Creates a grouped bar plot for TCAV scores for a specific target class.
     Displays the y-value (tcav_score) above each bar.
+
+    Parameters:
+    ----------
+    df : pandas.DataFrame
+        Dataframe containing TCAV scores and related information. It must have the following columns:
+        - 'concept_index': Unique identifier for each concept.
+        - 'concept_name': Name of each concept.
+        - 'tcav_score': TCAV score for each concept and layer.
+        - 'layer_name': The layer in the model for which the TCAV score was calculated.
+        - 'action': Target class or action label used to filter scores.
+
+    target_class_name : str
+        The name of the target class for which the TCAV scores are to be plotted.
+        Only TCAV scores corresponding to this class will be included in the plot.
+
+    show : bool, optional
+        If True, displays the plot using plt.show(). Defaults to True.
+
+    file_path_for_saving : str or None, optional
+        Path to save the generated plot as a .png file. If provided, the plot will be saved at this path.
+        If None, the plot is not saved. Defaults to None.
+
+    Raises:
+    ------
+    ValueError
+        If more than 5 unique concepts are present in the 'concept_index' column of `df`,
+        as the plot is designed to display a maximum of 5 concepts.
+
     """
     concept_indices = sorted(df['concept_index'].unique())
     if len(concept_indices) > 5:
@@ -46,6 +75,8 @@ def tcav_scores_barplot(df, target_class_name, show=True, file_path_for_saving=N
                      ha='center', va='bottom', fontsize=11, color='black')
 
     if file_path_for_saving:
+        directory = os.path.dirname(file_path_for_saving)
+        os.makedirs(directory, exist_ok=True)
         plt.savefig(file_path_for_saving, dpi=300, bbox_inches='tight')
 
     if show:
