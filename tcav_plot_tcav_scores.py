302df00e8c66c16d5bd4e054c42d6bc5f7dfb3e6
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-11-03 12:24:31 +0100

    Update plot tcav scores script

--- tcav_plot_tcav_scores.py
+++ tcav_plot_tcav_scores.py
@@ -1,35 +1,47 @@
 import pandas as pd
-from tcav_scores_barplot import tcav_scores_barplot
+from tcav import tcav_scores_barplot
 import gymnasium as gym
 import avocado_run
 
-TRAIN_RUN_NAME = "dutiful_frog_68"
-MODEL_NAME = "best_model"
-CONCEPTS_N = 4
 
-tcav_scores_df = pd.read_csv(
-    f'tcav_explanations/tcav_scores/{TRAIN_RUN_NAME}/{MODEL_NAME}/tcav_scores.csv')
+def plot_tcav_scores(train_run_name, model_name, tcav_score_data_type, batch, concepts_per_plot, target_classes):
+    tcav_scores_df = pd.read_csv(
+        f'tcav_explanations/tcav_scores/{train_run_name}/{model_name}/{tcav_score_data_type}_observations/tcav_scores_cav_batch_{batch}.csv')
 
-env = gym.make(id="AvocadoRun-v0", num_avocados=1, num_enemies=2)
-action_dict = env.unwrapped.action_dict
+    concept_indices = sorted(tcav_scores_df['concept_index'].unique())
+    concept_n = len(concept_indices)
+
+    for i in range(0, concept_n, concepts_per_plot):
+        # Create a subset of the concept indices for this partition, containing max 5 unique concepts
+        current_concept_indices = concept_indices[i:i + concepts_per_plot]
+
+        subset_df = tcav_scores_df[tcav_scores_df['concept_index'].isin(
+            current_concept_indices)]
+
+        for target_class in target_classes:
+            tcav_scores_barplot(
+                df=subset_df,
+                target_class_name=target_class,
+                show=False,
+                file_path_for_saving=f"tcav_explanations/tcav_scores/{train_run_name}/{model_name}/{tcav_score_data_type}_observations/plots_cav_batch_{batch}/tcav_barplot_target_class_{target_class}_concepts_{current_concept_indices}.png"
+            )
 
 
-target_classes = list(action_dict.values())
+if __name__ == "__main__":
+    train_run_name = "moonlit_coffin_71"
+    model_name = "best_model"
+    tcav_score_data_type = "model_specific"
+    batch = 0
+    concepts_per_plot = 4
 
-concept_indices = sorted(tcav_scores_df['concept_index'].unique())
-concept_n = len(concept_indices)
+    env = gym.make(id="AvocadoRun-v0", num_avocados=1, num_enemies=2)
+    target_classes = list(env.unwrapped.action_dict.values())
 
-for i in range(0, concept_n, CONCEPTS_N):
-    # Create a subset of the concept indices for this partition, containing max 5 unique concepts
-    current_concept_indices = concept_indices[i:i + CONCEPTS_N]
-
-    subset_df = tcav_scores_df[tcav_scores_df['concept_index'].isin(
-        current_concept_indices)]
-
-    for target_class in target_classes:
-        tcav_scores_barplot(
-            df=subset_df,
-            target_class_name=target_class,
-            show=True,
-            file_path_for_saving=f"tcav_explanations/tcav_scores/{TRAIN_RUN_NAME}/{MODEL_NAME}/tcav_barplot_target_class_{target_class}_concepts_{i}-{i + CONCEPTS_N - 1}.png"
-        )
+    plot_tcav_scores(
+        train_run_name=train_run_name,
+        model_name=model_name,
+        tcav_score_data_type=tcav_score_data_type,
+        batch=batch,
+        concepts_per_plot=concepts_per_plot,
+        target_classes=target_classes
+    )
