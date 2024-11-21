6ab92b287fde693a6fec69f30f87c70e00f9d451
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-11-21 22:37:47 +0100

    Add mango run concept detector to package

--- tcav/__init__.py
+++ tcav/__init__.py
@@ -1,4 +1,5 @@
 from .cass_obtainer import CASSObtainer
 from .concept_detector import ConceptDetector
+from .mango_run_concept_detector import MangoRunConceptDetector
 from .cassat_plot import cassat_plot
 from .tcav_scores_barplot import tcav_scores_barplot
